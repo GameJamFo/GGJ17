ed6ce1a042eb65c438cdb0a0a4eb49d1b08dda3a
Tordur <bboy_nightraven@hotmail.com>
2017-01-22 05:48:56 +0000

    Scene!

---
+++ Assets/lvl_2 Scene/scr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour {
+
+    Transform target;
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
+    float detectRange = 30;
+
+
+    // Use this for initialization
+    void Start () {
+        //door = this.gameObject;
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        var tgtDirection = target.position - transform.position;
+        var tgtDistance = tgtDirection.magnitude;
+        if (tgtDistance <= detectRange)
+        {
+            this.gameObject.GetComponent<Animation>().Play("Default Take");
+        }
+    }
+}
